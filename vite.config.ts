1db0f9b37572d7bc614bcf7076319572951ea255
MrNobody-MCMXCIX <pedrosilva.medeiros1820@gmail.com>
2026-09-25 19:53:12 -0300

    Atualizações feitas

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/orcafacil/",
+  base: mode === "production" ? "/orcafacil/" : "/",
 
   server: {
     host: "::",
